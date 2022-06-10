95716c9d1ac8a47eca91a8d010a2a69df3d4ac68
HuunterSG <agustinjperez.r@gmail.com>
2022-06-10 14:50:25 -0300

    rama lista queda incluir link de agustin

--- js/main.js
+++ js/main.js
@@ -10,18 +10,42 @@ $(document).ready(function(){
             }
         });
     });
-    $('.nav-quit').click(function(){
-        
-        $('#offcanvasNavbarLight').slideToggle(500, function(e){
+    
+      
+        $('.nav-quit').click(function(){
             
-            $('.offcanvas-backdrop').hide()
+            if($(window).width()<992){
+                $('#offcanvasNavbarLight').slideToggle(500)
+                $('.navbar-toggler').click(function(e){
+                    e.preventDefault()
+                    $('#offcanvasNavbarLight').show()
+                    
+                })
+            }
         })
+        const sections=document.querySelectorAll('.section')
+                const navQuit=document.querySelectorAll('.navDesktop')
+                console.log(navQuit)
+                console.log(sections)
+                window.onscroll = () => {
+                    var current = "";
+                
+                    sections.forEach((section) => {
+                    const sectionTop = section.offsetTop;
+                    if (pageYOffset >= sectionTop -100) {
+                        current = section.getAttribute("id"); }
+                        
+                    });
+                
+                    navQuit.forEach((a) => {
+                    a.classList.remove("active");
+                    if (a.classList.contains(current)) {
+                        a.classList.add("active");
+                    }
+                    });
+                };
+       
         
-    })
-    $('.navbar-toggler').click(function(){
-        $('#offcanvasNavbarLight').show()
-        
-    })
 });
 
 
