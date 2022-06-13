567292afeddae05a0d40c333e54a7261386fcdeb
HuunterSG <agustinjperez.r@gmail.com>
2022-06-13 10:15:19 -0300

    c

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             }
         });
     });
-
+          
         $('.nav-quit').click(function(){
             
             if($(window).width()<992){
@@ -24,6 +24,8 @@ $(document).ready(function(){
         })
         const sections=document.querySelectorAll('.section')
                 const navQuit=document.querySelectorAll('.navDesktop')
+                console.log(navQuit)
+                console.log(sections)
                 window.onscroll = () => {
                     var current = "";
                 
